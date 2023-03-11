4afc0175c300d6f42fb79c14c9924c8042dd32ad
Roman Vasilev <abeletskiy@corp.local>
2023-03-11 13:05:53 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Container from "@mui/material/Container";
 
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
-import {fetchAuthMe, selectIsAuth} from "./redux/slices/auth";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
 
 function App() {
-    const dispatch = useDispatch();
-    const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
 
-    React.useEffect(() => {
-        dispatch(fetchAuthMe());
-    }, [])
-
-
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, []);
 
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-          <Routes>
-              <Route  path="/" element={<Home />}/>
-              <Route  path="/posts/:id" element={<FullPost />}/>
-              <Route  path="/posts/:id/edit" element={<AddPost />}/>
-              <Route  path="/add-post" element={<AddPost />}/>
-              <Route  path="/login" element={<Login />}/>
-              <Route  path="/register" element={<Registration />}/>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/posts/:id/edit" element={<AddPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
       </Container>
     </>
   );
